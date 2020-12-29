6924967d4a988119df29cc08e08851a119750e34
Manish Soni <msoni@ar.iitr.ac.in>
2020-12-29 11:10:46 +0530

    changes 1

--- src/index.js
+++ src/index.js
@@ -13,7 +13,47 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World!");
+    res.status(200).send("Hello world!");
+});
+
+app.post('/add', (req, res) => {
+
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+
+    let response = {};
+
+    if (!req.body.num1 || !req.body.num2) {
+        response.status = "failure";
+        res.status(404).send(response);
+        return;
+    }
+
+    if (isNaN(num1) || isNaN(num2)) {
+        response.status = "error";
+        response.message = "Invalid data types";
+        res.status(400).send(response);
+        return;
+    }
+
+    if (num1>1000000 || num2>1000000) {
+        response.status = "error";
+        response.message = "Overflow";
+        res.status(400).send(response);
+        return;
+    }
+
+    if (num1<-1000000 || num2<-1000000) {
+        response.status = "error";
+        response.message = "Underflow";
+        res.status(400).send(response);
+        return;
+    }
+    response.status = "success";
+    response.message = "the sum of given two numbers";
+    response.result = Number(num1)+Number(num2);
+    console.log(req.body);
+    res.status(200).send(response);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
